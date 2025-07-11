ee942b7e6e217e0653c13479fd7426cdc45dd3de
Jennyfer <jennyknowles04@gmail.com>
2025-07-11 14:38:15 -0300

    :feat Create endpoint upload audio

--- src/http/routes/create-question.ts
+++ src/http/routes/create-question.ts
@@ -2,6 +2,8 @@ import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod';
 import { z } from 'zod/v4';
 import { db } from '../../db/connection.ts';
 import { schema } from '../../db/schema/index.ts';
+import { generateEmbeddings, generateAnswer } from '../../services/gemini.ts';
+import { and, eq, sql } from 'drizzle-orm';
 
 export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
     app.post('/rooms/:roomId/questions', {
@@ -10,7 +12,7 @@ export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
                 roomId: z.string(),
             }),
             body: z.object({
-                question: z.string().min(1),
+                question: z.string().min(1)
             }),
         },
     }, 
@@ -18,17 +20,48 @@ export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
         const { roomId } = request.params
         const { question } = request.body
 
+        const embeddings = await generateEmbeddings(question)
+
+        const embeddingsAsString = `[${embeddings.join(',')}]`
+
+        const chunks = await db
+        .select({
+            id: schema.audioChunks.id,
+            transcription: schema.audioChunks.transcription,
+            similarity: sql<number>`1 - (${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector)`
+        })
+        .from(schema.audioChunks)
+        .where(and(
+            eq(schema.audioChunks.roomId, roomId), 
+            sql`1 - (${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector) > 0.7`
+        ) )
+        .orderBy(sql`${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector`)
+        .limit(3)
+
+        // return chunks
+
+        let answer: string | null = null
+
+        if (chunks.length > 0) {
+            const transcriptions = chunks.map((chunk) => chunk.transcription)
+
+            answer = await generateAnswer(question, transcriptions)
+        }
+
         const result = await db
             .insert(schema.questions)
-            .values({ roomId, question })
+            .values({ roomId, question, answer })
             .returning()
 
-        const insertQuestion = result[0]
+        const insertedQuestion = result[0]
 
-        if(!insertQuestion){
+        if (!insertedQuestion) {
             throw new Error('Failed to create new room.')
         }
 
-        return reply.status(201).send({ questionId: insertQuestion })
+        return reply.status(201).send({
+            questionId: insertedQuestion.id,
+            answer,
+        })
     })
 }
--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,39 @@
-import { fastifyCors } from '@fastify/cors';
-import { fastify } from 'fastify';
-import { 
+import { fastifyCors } from '@fastify/cors'
+import { fastifyMultipart } from '@fastify/multipart'
+import { fastify } from 'fastify'
+import {
     serializerCompiler,
     validatorCompiler,
     type ZodTypeProvider,
-} from 'fastify-type-provider-zod';
-import { env } from './env.ts';
-import { getRoomsRoute } from './http/routes/get-rooms.ts';
-import { createRoomRoute } from './http/routes/create-room.ts';
-import { getRoomQuestions } from './http/routes/get-room-questions.ts';
-import { createQuestionRoute } from './http/routes/create-question.ts';
+} from 'fastify-type-provider-zod'
+import { env } from './env.ts'
+import { createQuestionRoute } from './http/routes/create-question.ts'
+import { createRoomRoute } from './http/routes/create-room.ts'
+import { getRoomQuestions } from './http/routes/get-room-questions.ts'
+import { getRoomsRoute } from './http/routes/get-rooms.ts'
+import { uploadAudioRoute } from './http/routes/upload-audio.ts'
 
-const app = fastify().withTypeProvider<ZodTypeProvider>();
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
     origin: 'http://localhost:5173',
 })
 
-app.setSerializerCompiler(serializerCompiler);
-app.setValidatorCompiler(validatorCompiler);
+app.register(fastifyMultipart)
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
 
 app.get('/health', () => {
-    return 'Ok'
+    return 'OK'
 })
 
 app.register(getRoomsRoute)
 app.register(createRoomRoute)
 app.register(getRoomQuestions)
 app.register(createQuestionRoute)
+app.register(uploadAudioRoute)
 
 app.listen({ port: env.PORT })
+
+// Paroui no minuto 35:58
